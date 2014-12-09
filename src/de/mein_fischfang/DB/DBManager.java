cfa94aa8bd194422b1a1e9193f0655f4e7557d0d
Matthias Stief <matthias.stief@gmx.net>
2014-12-09 16:47:38 +0100

    DBManager fertig gestellt

--- src/de/mein_fischfang/DB/DBManager.java
+++ src/de/mein_fischfang/DB/DBManager.java
@@ -41,8 +41,84 @@ public class DBManager extends SQLiteOpenHelper
      */
     public final void onCreate(SQLiteDatabase db)
     {
-        String sSQL = "";
+        // Tabelle FISH anlegen
+        String sSQL = "create table fish (name text)";
+        db.execSQL(sSQL);
 
+        // Fische hinzufügen
+        sSQL = "insert into fish (name) values ('Beispiel-Fisch')";
+        db.execSQL(sSQL);
+
+        // Tabelle BAIT (Köder) anlegen
+        sSQL = "create table bait (name text, type text)";
+        db.execSQL(sSQL);
+
+        // Köder hinzufügen
+        sSQL = "insert into bait (name, type) values ('Tauwurm', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Mistwurm', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Made', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Mais', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Köderfisch', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Boilie', 'Naturköder')";
+        db.execSQL(sSQL);
+        sSQL = "insert into bait (name, type) values ('Hundefutter', 'Naturköder')";
+        db.execSQL(sSQL);
+
+        // Tabelle WATERS (Gewässer) anlegen
+        sSQL = "create table waters (name text, type text, latitude real, longitude real)";
+        db.execSQL(sSQL);
+
+        // Tabelle ASSEMBLING (Montage) anlegen
+        sSQL = "create table assembling (name text)";
+        db.execSQL(sSQL);
+
+        // Montagen anlegen
+        sSQL = "insert into assembling (name) values ('Grund (Freilaufmontage)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('Grund (Selbsthakmontage)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('Pose (Grund)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('Pose (Freiwasser)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('Pose/Wasserkugel (Oberfläche)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('aktive Führung (Grund)')";
+        db.execSQL(sSQL);
+        sSQL = "insert into assembling (name) values ('aktive Führung (Freiwasser)')";
+        db.execSQL(sSQL);
+
+        // Tabelle WEATHER (Wetter) anlegen
+        sSQL = "create table weather (name text)";
+        db.execSQL(sSQL);
+
+        // Wetter anlegen
+        sSQL = "insert into weather (name) values ('klar')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('Nebel')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('leicht bewölkt')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('stark bewölkt')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('bedeckt')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('leichter Regen')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('starker Regen')";
+        db.execSQL(sSQL);
+        sSQL = "insert into weather (name) values ('Gewitter')";
+        db.execSQL(sSQL);
+
+        // Tabelle CATCH (Fang) anlegen
+        sSQL = "create table catch (time integer, size integer, weight integer, photo text, latitude real, longitude real, "
+                + "hint text, air_temperature real, water_temperature real, pressure real, feed integer, release integer, "
+                + "fish integer, bait integer, waters integer, assembling integer, weather integer)";
         db.execSQL(sSQL);
     }
 
